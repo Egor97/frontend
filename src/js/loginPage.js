fa6667a953e0c11feab721c836ab7fb16a5a4ebb
Egor Bizyaev <egorbizyaev123@gmail.com>
2021-05-20 10:30:24 +0400

    add: autorization, registration page

---
+++ src/js/loginPage.js
@@ -0,0 +1,9 @@
+var hiddenBox = $( "#inner__wrapper" );
+$( "#button__autorization" ).on("click", function( event ) {
+	hiddenBox.show();
+});
+
+
+$(".button__register").click(function() {
+    window.location.href='registrationPage.html';
+});
